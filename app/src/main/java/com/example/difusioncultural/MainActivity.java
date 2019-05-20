026784729806db46825425e7000e5fed09d8cd32
akre8590 <akre8590@gmail.com>
2019-05-20 16:27:10 -0500

    Login
    Finished login

--- app/src/main/java/com/example/difusioncultural/MainActivity.java
+++ app/src/main/java/com/example/difusioncultural/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.difusioncultural;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -29,13 +34,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_sign_out = (Button)findViewById(R.id.btn_sign_out);
+        btn_sign_out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance()
+                        .signOut(MainActivity.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                btn_sign_out.setEnabled(false);
+                                showsSignInOptions();
+
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
 
         //Init provider
         providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build(), // Email Builder
                 new AuthUI.IdpConfig.PhoneBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build()
-        );
+                new AuthUI.IdpConfig.GoogleBuilder().build());
 
         showsSignInOptions();
 
@@ -48,7 +72,8 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == MY_REQUEST_CODE){
             IdpResponse response = IdpResponse.fromResultIntent(data);
             if (resultCode == RESULT_OK){
-
+                Intent intent = new Intent(MainActivity.this, Events.class);
+                startActivity(intent);
                 //Get user
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 //Show email on Toast
@@ -65,8 +90,9 @@ public class MainActivity extends AppCompatActivity {
     public void showsSignInOptions(){
         startActivityForResult(
                 AuthUI.getInstance().createSignInIntentBuilder()
-                .setTheme(R.style.myTheme)
-                .build(), MY_REQUEST_CODE
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.myTheme)
+                        .build(), MY_REQUEST_CODE
         );
     }
 
